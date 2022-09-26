39c4c4d87fe4e5a6209020b147920cb1d183d028
Kevin Reynaldo <kevin@taptalk.io>
2022-09-26 18:52:13 +0700

    • Fixed getUrlsFromString regex

--- index.js
+++ index.js
@@ -980,7 +980,8 @@ exports.taptalkHelper = {
 
     getUrlsFromString(string) {
         // const expression = /[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)?/gi;
-        const expression = /(\b((https?|ftp|file):\/\/)?[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
+        // const expression = /(\b((https?|ftp|file):\/\/)?[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
+        const expression = /(\b((https?|ftp|file):\/\/)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)?)/gi;
         const regex = new RegExp(expression);
         const urls = string.match(regex);
         if (urls !== null && urls !== undefined) {
